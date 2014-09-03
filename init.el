090588d49e1ed2660e633fb980c34302aa860311
gin0606 <kkgn06@gmail.com>
2014-09-03 23:50:56 +0900

    quelpa入れた

--- init.el
+++ init.el
@@ -1,5 +1,13 @@
 ;;; init.el --- 最低限のものを入れる
 ;;; Commentary:
 ;;; Code:
+;; quelpa
+(package-initialize)
+(if (require 'quelpa nil t)
+    (defvar quelpa-update-melpa-p nil)
+    (quelpa-self-upgrade)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.github.com/quelpa/quelpa/master/bootstrap.el")
+    (eval-buffer)))
 (provide 'init)
 ;;; init.el ends here
