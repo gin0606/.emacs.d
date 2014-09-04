60dbeaa7ec30babea5ee7a3a0b16d059ab93f345
gin0606 <kkgn06@gmail.com>
2014-09-04 11:11:14 +0900

    雑多な設定いろいろ

--- init.el
+++ init.el
@@ -45,5 +45,34 @@
 (set-fontset-font (frame-parameter nil 'font)
                   'katakana-jisx0201
                   (cons font-name font-name))
+
+(setq inhibit-startup-message t)	; 起動時のスプラッシュページを表示しない
+(tool-bar-mode 0)			; ツールバー非表示
+
+(setq backup-inhibited t)		; バックアップファイルを作らない
+(setq delete-auto-save-files t)		; AutoSaveファイルを消す
+
+(setq truncate-lines t)			; 行の折り返しをしない
+
+(define-key global-map [ns-drag-file] 'ns-find-file) ; D&Dでファイル開く
+(setq ns-pop-up-frames nil)                          ; D&Dで新規フレームを開かない
+
+(defvar ls-lisp-dirs-first t)           ; ディレクトリから先に表示
+
+; 対応するカッコを光らせる
+(show-paren-mode 1)
+
+(global-hl-line-mode t)
+(set-face-background 'hl-line "#555522")
+
+(setq frame-title-format '("%b"))
+
+; 行数表示
+(global-linum-mode t)
+(defvar linum-format "%4d")
+
+(column-number-mode t)
+
+
 (provide 'init)
 ;;; init.el ends here
