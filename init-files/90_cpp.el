2f64330e8b8cc45900add66555e7c0e00ce5d3f5
gin0606 <kkgn06@gmail.com>
2014-09-05 21:20:12 +0900

    Xcodeからcflags取得してくれるやつ入れた
    https://github.com/typester/clang-complete-helper

--- init-files/90_cpp.el
+++ init-files/90_cpp.el
@@ -20,6 +20,11 @@
   (global-auto-complete-mode t))
 (my-ac-config)
 
+(setq ac-clang-cflags
+      (split-string
+       (shell-command-to-string (concat (executable-find "clang-complete-helper") " cflags "
+                                        (and buffer-file-name
+                                             (file-relative-name buffer-file-name))))))
 
 (provide '90_cpp)
 ;;; 90_cpp.el ends here
