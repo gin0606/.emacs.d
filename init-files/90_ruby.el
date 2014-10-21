7d59b70eb10c71e67b438c254a913cd2b2fd4be7
gin0606 <kkgn06@gmail.com>
2014-10-21 12:51:28 +0900

    Rubyの設定を修正した

--- init-files/90_ruby.el
+++ init-files/90_ruby.el
@@ -3,10 +3,26 @@
 ;;; Code:
 (require 'ruby-mode)
 (add-hook 'ruby-mode-hook
-          '(lambda ()
-             (setq ruby-indent-level tab-width)
-             (setq ruby-deep-indent-paren-style nil)
-             (define-key ruby-mode-map [return] 'ruby-reindent-then-newline-and-indent)))
+	  '(lambda ()
+	     (setq tab-width 2)
+	     (setq ruby-indent-level tab-width)
+	     (setq ruby-deep-indent-paren-style nil)
+	     (define-key ruby-mode-map [return] 'reindent-then-newline-and-indent)))
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  "Rubyのindentをいい感じにしてくれる http://willnet.in/13 ."
+  (let ((column (current-column))
+	indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+	(setq offset (- column (current-column)))
+	(when (and (eq (char-after) ?\))
+		   (not (zerop (car state))))
+	  (goto-char (cadr state))
+	  (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
 
 (quelpa 'ruby-end)
 (require 'ruby-end)
